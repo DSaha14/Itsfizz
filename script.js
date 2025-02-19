a83593ec01728a2d80edbffdaa466164552c1706
DSaha14 <dishasaha1404@gmail.com>
2025-02-20 00:59:32 +0530

    $1

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 
-const gsap = require("gsap");
-gsap.from('.hero-title', {
-    opacity: 0,
-    y: 20,
-    duration: 1,
-    delay: 0.5,
-  });
-  
-  gsap.from('.hero-subtitle', {
-    opacity: 0,
-    y: 20,
-    duration: 1,
-    delay: 1,
-  });
+const gsap = require("gsap").gsap; // Extract gsap from the module
+
+gsap.from(".element", { opacity: 0, y: -50, duration: 1 });
+
+gsap.from(".hero-title", {
+  opacity: 0,
+  y: 20,
+  duration: 1,
+  delay: 0.5,
+});
+
+gsap.from(".hero-subtitle", {
+  opacity: 0,
+  y: 20,
+  duration: 1,
+  delay: 1,
+});
