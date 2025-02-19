60062ca04cbbb7d67ab98bf8512218e9097715e7
DSaha14 <dishasaha1404@gmail.com>
2025-02-19 23:22:55 +0530

    Initial code upload

---
+++ script.js
@@ -0,0 +1,13 @@
+gsap.from('.hero-title', {
+    opacity: 0,
+    y: 20,
+    duration: 1,
+    delay: 0.5,
+  });
+  
+  gsap.from('.hero-subtitle', {
+    opacity: 0,
+    y: 20,
+    duration: 1,
+    delay: 1,
+  });
